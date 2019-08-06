6dec864d00cb4085a5bdfc61701b3e49a4cf230a
Jeremiah <almostgone47@hotmail.com>
2019-08-06 10:31:15 +1000

    completed gub finder

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -23,17 +23,23 @@ const Search = ({setAlert}) => {
     return (
       <div>
         <form className="form" onSubmit={onSubmit}>
-          <input 
+          <div class="row" style={bannerStyles}>
+            <img src="images/github.png" style={{width: "40%"}} />
+            <h1>Search any username you'd like to find on Github.</h1>
+            <input 
             type="text" 
             name="text"
             placeholder="Search Users..."
             value={text}
             onChange={onChange}
+            style={{width:'70%'}}
             />
-          <input 
+            <input 
             type="submit"
             value="Search"
-            className="btn btn-dark btn-block" />
+            className="btn btn-dark btn-block"
+            style={{width:'70%'}} />
+          </div>
         </form>
         {githubContext.users.length > 0 && (
           <button 
@@ -44,6 +50,12 @@ const Search = ({setAlert}) => {
         )}
       </div>
     )
+  }
+
+  const bannerStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
   }
 
 export default Search
